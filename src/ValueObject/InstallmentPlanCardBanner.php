d3e45edc1f0599a90f4c8a262c2d4a532b905fe8
Your Name <you@example.com>
2026-03-31 14:08:11 -0300

    Corrige cálculo do parcelamento via WS para visa e elo

--- src/ValueObject/InstallmentPlanCardBanner.php
+++ src/ValueObject/InstallmentPlanCardBanner.php
@@ -9,6 +9,16 @@ class InstallmentPlanCardBanner
     private $mastercard;
 
     /**
+     * @var InstallmentPlanCardBannerContainer
+     */
+    private $visa;
+
+    /**
+     * @var InstallmentPlanCardBannerContainer
+     */
+    private $elo;
+
+    /**
      * Get the value of mastercard
      *
      * @return  InstallmentPlanCardBannerContainer
@@ -28,6 +38,54 @@ class InstallmentPlanCardBanner
     public function setMastercard(InstallmentPlanCardBannerContainer $mastercard)
     {
         $this->mastercard = $mastercard;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of visa
+     *
+     * @return  InstallmentPlanCardBannerContainer
+     */ 
+    public function getVisa()
+    {
+        return $this->visa;
+    }
+
+    /**
+     * Set the value of visa
+     *
+     * @param  InstallmentPlanCardBannerContainer  $visa
+     *
+     * @return  self
+     */ 
+    public function setVisa(InstallmentPlanCardBannerContainer $visa)
+    {
+        $this->visa = $visa;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of elo
+     *
+     * @return  InstallmentPlanCardBannerContainer
+     */ 
+    public function getElo()
+    {
+        return $this->elo;
+    }
+
+    /**
+     * Set the value of elo
+     *
+     * @param  InstallmentPlanCardBannerContainer  $elo
+     *
+     * @return  self
+     */ 
+    public function setElo(InstallmentPlanCardBannerContainer $elo)
+    {
+        $this->elo = $elo;
 
         return $this;
     }
